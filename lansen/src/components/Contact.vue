46f367bb0c474ffe3ff9aa751306cbe2b1437cb9
Adam Hartline <arcydrool@gmail.com>
2025-06-13 18:47:31 -0400

    Get that contact info in there

---
+++ lansen/src/components/Contact.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import {ref} from 'vue';
+const mailLink = ref('info' + '@'.repeat(1) + 'lansen.com');
+</script>
+
+<template>
+    <div id="contact">
+    1 Main St., Berkshire MA 01224<br>
+    Mail: P.O. Box 1481<br>
+    Lanesboro, MA 01237<br>
+    Tel:(413)443-5328 - Fax: (413)443-2688<br>
+    <a v-bind:href="'mailto:' + mailLink">{{ mailLink }}</a>
+    </div>
+</template>
