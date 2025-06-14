fa465407bfd883dd142f39abb9fb8c86b0f54af9
Adam Hartline <arcydrool@gmail.com>
2025-06-14 15:13:28 -0400

    Decorations

--- lansen/src/App.vue
+++ lansen/src/App.vue
@@ -1,10 +1,32 @@
 <script setup lang="ts">
+import {computed, reactive } from 'vue'
+import { useHead } from '@vueuse/head'
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
+const siteData = reactive({
+  title: 'Lansen Mold - Custom injection molder; mold design, manufacturing and repair; molding ISO and ASTM test specimens',
+  description: 'Custom Injection molders supplying the plastics industry with cost effective solutions to molding and tooling needs.',
+  keywords: ["plastics", "injection molding", "mold making", "mould making", "mould design", "mold design and engineering", "machining", "tooling", "injection molding", "tool and die", "die shop", "die maker", "plastic molds", "plastic moulds", "platic dies", "izod", "ASTM", "test", "testing", "IZOD", "notched izod"]
+})
+useHead({
+  // Can be static or computed
+  title: computed(() => siteData.title),
+  meta: [
+    {
+      name: 'description',
+      content: computed(() => siteData.description),
+    },
+    { name: 'keywords',
+      content: computed(() => siteData.keywords.join(", ")),
+    }
+  ],
+})
 </script>
 
 <template>
   <header>
+
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
--- lansen/src/components/Contact.vue
+++ lansen/src/components/Contact.vue
@@ -4,7 +4,7 @@ const mailLink = ref('info' + '@'.repeat(1) + 'lansen.com');
 </script>
 
 <template>
-    <div id="contact">
+    <div id="contact" class="footer">
     1 Main St., Berkshire MA 01224<br>
     Mail: P.O. Box 1481<br>
     Lanesboro, MA 01237<br>
@@ -12,3 +12,18 @@ const mailLink = ref('info' + '@'.repeat(1) + 'lansen.com');
     <a v-bind:href="'mailto:' + mailLink">{{ mailLink }}</a>
     </div>
 </template>
+
+<style scoped>
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  border: 4px solid #2f2328;
+  background-color: #1f1318;
+  color: white;
+  text-align: center;
+  font-family: Tahoma;
+  font-size: small;
+}
+</style>
