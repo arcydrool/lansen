266bc2d53654f4dd0629c1d7b0a28cbd8847162d
Adam Hartline <arcydrool@gmail.com>
2025-06-18 11:53:28 -0400

    Added Specimen

--- lansen/src/views/SpecimenView.vue
+++ lansen/src/views/SpecimenView.vue
@@ -1,3 +1,35 @@
+<script setup lang="ts">
+import Card from 'primevue/card';
+import { RouterLink } from 'vue-router';
+import Image from 'primevue/image';
+import Ispec from '../assets/spec1.jpg';
+</script>
 <template>
-  <div>This is Specimen</div>
+  <h1>Specimen Molding</h1>
+  <Card>
+    <template #title>Specimen molding for the testing industry:</template>
+    <template #content>
+      <ul>
+        <li>We provide quick and thorough services for
+          the testing industry. Our standard delivery is two to three weeks. Quicker deliveries
+          maybe available after a project review.&nbsp;</li>
+        <li>We respond to all inquirers within 24 hours.
+          Please fill out our <router-link to="/quote"> online quotation form</router-link>
+          or call John Rosier at <a href="tel:+14134435328">(413)443-5328</a>.</li>
+        <li>ISO and ASTM family molds are balanced per
+          standards on mold design ISO 294 and ASTM D3641.</li>
+        <li>Pricing starting at $390.00, includes 2 hours
+          of processing, tool setup, material handling and completed ?as molded? process
+          sheet for each material type.&nbsp;</li>
+        <li>Over 45 years experience in molding test specimens.</li>
+        <li>Our injection molding machines and inspection equipment are calibrated and
+          certified yearly.</li>
+        <li>Complete in house tooling facilities for special
+          mold manufacturing and special machining services.</li>
+        <li>If you need specimen testing we will direct
+          you to a qualified testing laboratory.</li>
+      </ul>
+      <Image :src="Ispec" alt="Lansen Mold offers a variety of products."/>
+    </template>
+  </Card>
 </template>
