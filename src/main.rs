e2ea354f6f2f0834351cbee1da6cc76902af632d
Adam Hartline <arcydrool@gmail.com>
2025-07-21 19:07:13 -0400

    Restore main

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ extern crate rocket;
 #[cfg(test)]
 mod tests;
 
-mod model;
 mod contact;
 mod mail;
+mod model;
 mod moldspec;
 
 use rocket::fs::{relative, FileServer};
@@ -17,24 +17,20 @@ fn index() -> Redirect {
     Redirect::to(uri!("/index.html", moldspec::list()))
 }
 
-#[rocket::main]
-async fn main() -> Result<(), rocket::Error> {
+#[launch]
+fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
         .mount(
             "/moldspec",
-            routes![
-                moldspec::create,
-                moldspec::list,
-                moldspec::read
-            ],
+            routes![moldspec::create, moldspec::list, moldspec::read],
         )
         .register("/c", catchers![contact::default])
         .mount("/c", routes![contact::create])
         .mount(
             "/",
             FileServer::new(relative!("static"), rocket::fs::Options::None),
-        ).attach(model::stage())
+        )
+        .attach(model::stage())
         .attach(mail::stage())
-        .launch.await
 }
